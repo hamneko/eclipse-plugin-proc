f1572d92a8aa33de831fcff1d424603ed5addba9
Jun Ishikawa <hamnyan@outlook.com>
2025-09-15 10:39:37 +0900

    #3 Add main features

--- src/plugin/proc/PROCLanguage.java
+++ src/plugin/proc/PROCLanguage.java
@@ -1,9 +1,28 @@
 package plugin.proc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.gnu.c.GCCLanguage;
+import org.eclipse.cdt.core.dom.parser.c.GCCParserExtensionConfiguration;
+import org.eclipse.cdt.core.dom.parser.c.ICParserExtensionConfiguration;
+import org.eclipse.cdt.core.index.IIndex;
 import org.eclipse.cdt.core.model.ICLanguageKeywords;
+import org.eclipse.cdt.core.parser.FileContent;
+import org.eclipse.cdt.core.parser.IParserLogService;
+import org.eclipse.cdt.core.parser.IScanner;
+import org.eclipse.cdt.core.parser.IScannerInfo;
+import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
+import org.eclipse.cdt.core.parser.ParserMode;
+import org.eclipse.core.runtime.CoreException;
+
+import plugin.common.parser.scanner.ExecSqlPosition;
+import plugin.proc.parser.scanner.PROCScanner;
 
 public class PROCLanguage extends GCCLanguage {
+
+	private List<ExecSqlPosition> execSqlPositions;
 
 	public PROCLanguage() {
 	}
@@ -32,4 +51,41 @@ public class PROCLanguage extends GCCLanguage {
 		return result;
 	}
 
+	@Override
+	protected IScanner createScanner(FileContent content, IScannerInfo scanInfo, IncludeFileContentProvider fcp,
+			IParserLogService log) {
+		execSqlPositions = new ArrayList<ExecSqlPosition>();
+		return new PROCScanner(content, scanInfo, getParserLanguage(), log, getScannerExtensionConfiguration(scanInfo),
+				fcp, execSqlPositions);
+	}
+
+	@Override
+	public IASTTranslationUnit getASTTranslationUnit(FileContent reader, IScannerInfo scanInfo,
+			IncludeFileContentProvider fileCreator, IIndex index, int options, IParserLogService log)
+			throws CoreException {
+		return getASTTranslationUnit(reader, scanInfo, fileCreator, index, options, log, false);
+	}
+
+	@SuppressWarnings("restriction")
+	public IASTTranslationUnit getASTTranslationUnit(FileContent reader, IScannerInfo scanInfo,
+			IncludeFileContentProvider fileCreator, IIndex index, int options, IParserLogService log,
+			boolean calledFromFormatter) throws CoreException {
+
+		IScanner scanner = createScanner(reader, scanInfo, fileCreator, log);
+		scanner.setComputeImageLocations((options & OPTION_NO_IMAGE_LOCATIONS) == 0);
+		scanner.setProcessInactiveCode((options & OPTION_PARSE_INACTIVE_CODE) != 0);
+
+		ParserMode mode;
+		if ((options & OPTION_SKIP_FUNCTION_BODIES) != 0) {
+			mode = ParserMode.STRUCTURAL_PARSE;
+		} else {
+			mode = ParserMode.COMPLETE_PARSE;
+		}
+		ICParserExtensionConfiguration conf = GCCParserExtensionConfiguration.getInstance();
+
+		if (calledFromFormatter) {
+			return new PROCSourceParser(scanner, mode, log, conf, index, execSqlPositions).parse();
+		}
+		return new PROCSourceParser(scanner, mode, log, conf, index).parse();
+	}
 }
---
+++ src/plugin/proc/PROCSourceParser.java
@@ -0,0 +1,55 @@
+package plugin.proc;
+
+import java.util.List;
+
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
+import org.eclipse.cdt.core.dom.parser.c.ICParserExtensionConfiguration;
+import org.eclipse.cdt.core.index.IIndex;
+import org.eclipse.cdt.core.parser.EndOfFileException;
+import org.eclipse.cdt.core.parser.IParserLogService;
+import org.eclipse.cdt.core.parser.IScanner;
+import org.eclipse.cdt.core.parser.ParserMode;
+import org.eclipse.cdt.internal.core.dom.parser.ASTNode;
+import org.eclipse.cdt.internal.core.dom.parser.BacktrackException;
+import org.eclipse.cdt.internal.core.dom.parser.c.GNUCSourceParser;
+
+import plugin.common.parser.scanner.ExecSqlPosition;
+
+@SuppressWarnings("restriction")
+public class PROCSourceParser extends GNUCSourceParser {
+
+	private List<ExecSqlPosition> execSqlPositions;
+
+	public PROCSourceParser(IScanner scanner, ParserMode parserMode, IParserLogService logService,
+			ICParserExtensionConfiguration config, IIndex index) {
+		this(scanner, parserMode, logService, config, index, null);
+	}
+
+	public PROCSourceParser(IScanner scanner, ParserMode parserMode, IParserLogService logService,
+			ICParserExtensionConfiguration config, IIndex index, List<ExecSqlPosition> execSqlPositions) {
+		super(scanner, parserMode, logService, config, index);
+		this.execSqlPositions = execSqlPositions;
+	}
+
+	@Override
+	protected IASTStatement statement() throws EndOfFileException, BacktrackException {
+		IASTStatement statement = super.statement();
+		if (execSqlPositions == null) {
+			return statement;
+		}
+		ASTNode node = (ASTNode) statement;
+		if (inPosition(node.getOffset())) {
+			return statement();
+		}
+		return statement;
+	}
+
+	private boolean inPosition(int pos) {
+		for (ExecSqlPosition execSqlPosition : execSqlPositions) {
+			if (execSqlPosition.getFrom() <= pos && (execSqlPosition.getTo() - 2) >= pos) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
